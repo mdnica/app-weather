354b1b9eccea3561eb8ce053368fd85339903597
Diana Nica <118007843+mdnica@users.noreply.github.com>
2022-12-14 16:08:12 +0000

    weather icon updates

--- src/index.js
+++ src/index.js
@@ -55,6 +55,13 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#speed");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
